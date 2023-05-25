6b4d32fba2917e44fecdac67919da79c18de54fc
dungnt2khust <eddieonthecode@gmail.com>
2023-05-25 21:54:25 +0700

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Link from "next/link";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
 	return (
 		<>
+      <Head>
+        <title>Basic SEO and Rendering Strategies</title>
+        <link rel="sitemap" href="https://basic-seo-and-rendering-strategies.vercel.app/sitemap.xml" />
+      </Head>
 			<main className={`${styles.main} ${inter.className}`}>
         <h1>Basic SEO and Rendering Strategies</h1>
 				<div className={styles.grid}>
