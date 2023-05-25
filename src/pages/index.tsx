a844c052d695630b84efdbc5209fd8aa82e5209e
dungnt2khust <eddieonthecode@gmail.com>
2023-05-25 22:17:49 +0700

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,47 +8,44 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title>Basic SEO and Rendering Strategies</title>
-				{/* <link
-					rel="sitemap"
-					href="https://basic-seo-and-rendering-strategies.vercel.app/sitemap.xml"
-				/> */}
-			</Head>
+      <Head>
+        <title>Basic SEO and Rendering Strategies</title>
+        <link rel="sitemap" href="https://basic-seo-and-rendering-strategies.vercel.app/sitemap.xml" />
+      </Head>
 			<main className={`${styles.main} ${inter.className}`}>
-				<h1>Basic SEO and Rendering Strategies</h1>
+        <h1>Basic SEO and Rendering Strategies</h1>
 				<div className={styles.grid}>
-					<a href="https://basic-seo-and-rendering-strategies.vercel.app/sitemap.xml">SiteMap</a>
 					<Link href={"/seo"} className={styles.card}>
 						<h2>
 							SEO <span>-&gt;</span>
 						</h2>
-						<p>Search Engine Optimization</p>
+            <p>Search Engine Optimization</p>
 					</Link>
-					<Link href={"/csr"} className={styles.card}>
+          <Link href={"/csr"} className={styles.card}>
 						<h2>
 							CSR <span>-&gt;</span>
-							<p>Client Site Rendering</p>
+              <p>Client Site Rendering</p>
 						</h2>
 					</Link>
-					<Link href={"/ssr"} className={styles.card}>
+          <Link href={"/ssr"} className={styles.card}>
 						<h2>
 							SSR <span>-&gt;</span>
-							<p>Server Site Rendering</p>
+              <p>Server Site Rendering</p>
 						</h2>
 					</Link>
-					<Link href={"/ssg"} className={styles.card}>
+          <Link href={"/ssg"} className={styles.card}>
 						<h2>
 							SSG <span>-&gt;</span>
-							<p>Static Site Generation</p>
+              <p>Static Site Generation</p>
 						</h2>
 					</Link>
-					<Link href={"/isr"} className={styles.card}>
+          <Link href={"/isr"} className={styles.card}>
 						<h2>
 							ISR <span>-&gt;</span>
-							<p>Incremental Static Regeneration</p>
+              <p>Incremental Static Regeneration</p>
 						</h2>
 					</Link>
+					
 				</div>
 			</main>
 		</>
--- src/pages/seo.tsx
+++ src/pages/seo.tsx
@@ -1,13 +1,62 @@
 import { BackHome } from "@/components";
-import React from "react";
+import Head from "next/head";
+import React, { useState } from "react";
 
 type Props = {};
 
 export default function SEO({}: Props) {
+	const [seoData, setSeoData] = useState({
+		title: "SEO trang web của bạn nhanh chóng với chỉ 200k",
+		description: "Chỉ với 200k bạn sẽ đưa trang web của mình lên top 3 và người dùng không thể cưỡng lại mà click vào ngay !!!",
+		domain: "basic-seo-and-rendering-strategies.vercel.app",
+		url: "https://basic-seo-and-rendering-strategies.vercel.app",
+		image: "https://blog.shopline.com.vn/wp-content/uploads/2022/01/a1d39b9944184055866844a5d88fdec3.png"
+	});
 	return (
 		<>
+			<Head>
+				<title>{seoData.title}</title>
+				<meta
+					name="description"
+					content={seoData.description}
+				/>
+				<meta name="robots" content="index,follow"></meta>
+				<meta property="og:url" content={seoData.url}></meta>
+				<meta property="og:type" content="website"></meta>
+				<meta
+					property="og:title"
+					content={seoData.title}
+				></meta>
+				<meta
+					property="og:description"
+					content={seoData.description}
+				></meta>
+				<meta
+					property="og:image"
+					content={seoData.image}
+				></meta>
+
+				<meta name="twitter:card" content="summary_large_image"></meta>
+				<meta property="twitter:domain" content={seoData.domain}></meta>
+				<meta
+					property="twitter:url"
+					content={seoData.url}
+				></meta>
+				<meta
+					name="twitter:title"
+					content={seoData.title}
+				></meta>
+				<meta
+					name="twitter:description"
+					content={seoData.description}
+				></meta>
+				<meta
+					name="twitter:image"
+					content={seoData.image}
+				></meta>
+			</Head>
 			<BackHome></BackHome>
-			<div>SEO</div>
+			<h1>Content</h1>
 		</>
 	);
 }
